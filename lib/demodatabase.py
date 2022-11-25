654585827095e7fdfe973b7dbeeb3bf283e6f710
Jelle-1035608 <Benardjelle@gmail.com>
2022-11-25 12:40:13 +0100

    trying to create log in database

--- lib/demodatabase.py
+++ lib/demodatabase.py
@@ -1,21 +1,19 @@
 import sqlite3
 
-def create_login_database(login_database_file):
-    try:
-        connection = sqlite3.connect(login_database_file)
-    cursor = connection.cursor()
+connection = sqlite3.connect("login.db")
+cursor = connection.cursor()
 
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS demo (id PRIMARY KEY, username TEXT,password TEXT)"
+cursor.execute(
+    "CREATE TABLE login (username TEXT, password TEXT)"
     )
 
-    for pair in [
-        ("Veysel", "Altinok", "v.k.altinok@hr.nl"),
-        ("Diederik", "de Vries", "d.de.vries@hr.nl"),
-        ("Mark", "Otting", "m.b.otting@hr.nl"),
-    ]:
-        cursor.execute(
-            "INSERT INTO demo (first_name, last_name, email) VALUES (?, ?, ?)", pair
-        )
+for login_list in [
+    ("Jelle1", "ABC"),
+    ("Jelle2", "123")
+]:
+    cursor.executemany(
+        "INSERT INTO login VALUES (?, ?)", [login_list]
+    )
+
     connection.commit()
-    connection.close()
+connection.close()
