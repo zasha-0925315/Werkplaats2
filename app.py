5738a4ed0390e9228b3202dba9bf705f83bef9b4
Jelle-1035608 <Benardjelle@gmail.com>
2022-12-14 14:33:58 +0100

    added sessions to check if logged in on each page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os.path
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 
 from lib.tablemodel import DatabaseModel
 from lib.Login_details import Login_details
@@ -12,6 +12,7 @@ FLASK_PORT = 81
 FLASK_DEBUG = True
 
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 # This command creates the "<application directory>/databases/testcorrect_vragen.db" path
 DATABASE_FILE = os.path.join(app.root_path, 'databases', 'testcorrect_vragen.db')
 
@@ -20,11 +21,18 @@ if not os.path.isfile(DATABASE_FILE):
     print(f"Could not find database {DATABASE_FILE}, creating a demo database.")
 dbm = DatabaseModel(DATABASE_FILE)
 
+@app.before_request
+def before_request():
+    if "logged_in" not in session and request.endpoint not in ['login', 'static', 'login_index']:
+        return redirect(url_for('login_index'))
 
 # This is the main route that shows the login page
 @app.route("/")
 def login_index():
-    return render_template('login.html')
+    if "logged_in" in session:
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
 
 
 # Route that handles the login form
@@ -39,6 +47,7 @@ def login():
         if Username in data:
             if Password == data[Username]:
                 # If the username and password are correct, redirect to the main page
+                session['logged_in'] = True
                 return redirect(url_for('index'))
             else:
                 # If the password is incorrect, return to the login page
